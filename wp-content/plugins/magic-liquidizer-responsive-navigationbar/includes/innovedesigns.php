ed93d39917eefc9e8a9ff359cd397aa1d68a724c
Geoffrey <geoffrey.e@prodigy.net>
2014-10-18 21:57:05 -0600

    combine work

---
+++ wp-content/plugins/magic-liquidizer-responsive-navigationbar/includes/innovedesigns.php
@@ -0,0 +1,21 @@
+<?php
+defined( 'ABSPATH' ) OR exit;
+
+/*
+/--------------------------------------------------------------------\
+|                                                                    |
+| License: GPL Version 3                                             |
+|                                                                    |
+| Magic Liquidizer Responsive Form - Make HTML Form Responsive.      |
+| Copyright (C) 2014, Elvin Deza,                                    |
+| http://innovedesigns.com/                                          |
+| All rights reserved.                                               |
+|                                                                    |
+| By using the software, you agree to be bound by the terms of		 | 		
+| this license.														 |
+| 																	 |
+|                                                                    |
+\--------------------------------------------------------------------/
+*/
+
+// $txt=get_option('liquidizer_lite_wp_lisensya_key'); $re1='(id)'; $re2='.*?'; $re3='\\d+'; $re4='.*?'; $re5='(\\d+)'; if($c=preg_match_all ("/".$re1.$re2.$re3.$re4.$re5."/is", $txt, $matches)){ $word1=$matches[1][0]; $int1=$matches[2][0]; } 
